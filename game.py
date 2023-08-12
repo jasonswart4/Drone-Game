8d9d42aea148a01ec830f0facd8f552c06e6216e
jason.swart <jasonswart4@gmail.com>
2023-08-12 17:09:03 +1000

    random targets

--- game.py
+++ game.py
@@ -7,15 +7,15 @@ character_size = [64, 64]
 screen_size = [1500, 790]
 game = Game(screen_size, character_size)
 
-targets = [[0,20], [100,30], [-10,20], [50,10], [0,10]]
-#targets = [[uniform(-screen_size[0]/2,screen_size[0]/2), uniform(-screen_size[1]/2,screen_size[1]/2)] for _ in range(30)]
+#targets = [[0,20], [100,30], [-10,20], [50,10], [0,10]]
+targets = [[uniform(-100,100), uniform(-100,100)] for _ in range(30)]
 drones = [Drone(0,[[0,0,0],[0,0,0],[0,0,0]],targets)]
 scores = [0]
 max_score = 1e6
 game.break_angle = 1e6
 game.max_counter = 1e6
 game.max_dist = 2000
-drones[0].dt = 0.01
+drones[0].dt = 0.005
 
 run_test(game, drones, max_score)
 plt.plot(drones[0].thrust[0])
